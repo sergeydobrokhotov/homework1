1c261e0114e81c8a7b55753faa598c0540f7bf02
Sergey Dobrokhotov <sergey.dobrokhotov@realize.dev>
2019-12-06 12:51:37 +0300

    Пока сделал две задачи

--- homework2/App.java
+++ homework2/App.java
@@ -1,19 +1,23 @@
 package geekbrains.homework2;
 
 
-
 public class App {
     public static void main(String[] args) {
 //       задание 1
         changeArray();
 //        задание 2
         createArray();
+        createMassiv();
     }
 
 
-    //   Задать целочисленный массив, состоящий из элементов 0 и 1.
+//       Задать целочисленный массив, состоящий из элементов 0 и 1.
 //   Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
 //   С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+
+    //     Задать пустой целочисленный массив размером 8.
+    //     С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
     private static void changeArray() {
         System.out.println("Задание 1.");
         int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
@@ -43,7 +47,16 @@ public class App {
     }
 
 
+    private static void  createMassiv(){
+        System.out.println("\nЗадание 3.");
+        int[] arr = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.print("Неизменнный: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
 
+        }
+
+    }
 }
 
 
