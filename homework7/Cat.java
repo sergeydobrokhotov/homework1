9f478ace387e449c48682ea8f55715229931b911
Sergey Dobrokhotov <sergey.dobrokhotov@realize.dev>
2019-12-25 08:55:33 +0300

    deleted unnecessary file

--- homework7/Cat.java
+++ homework7/Cat.java
@@ -1,4 +1,29 @@
 package geekbrains.homework1.homework7;
 
 public class Cat {
+    private String name;
+    private int apppetite;
+    private boolean isFull;
+
+    Cat(String name, int apppetite) {
+        this.name = name;
+        this.apppetite = apppetite;
+        this.isFull = false;
+    }
+
+   @Override
+    public String toString() {
+        return "CAT: " + name + " | APPETITE: " + apppetite +"| ISFULL: " + isFull;
+   }
+
+
+    void eat(Plate plate) {
+        if (plate.getAmountOfFood() >= apppetite) {
+            plate.decreaseFood(apppetite);
+            isFull = true;
+        }
+    }
+
+
+
 }
--- homework7/Main.java
+++ homework7/Main.java
@@ -1,4 +1,26 @@
 package geekbrains.homework1.homework7;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("List of cats:");
+        Cat[] x = {new Cat("Matroskin", 56), new Cat("Tom", 78), new Cat("Ben", 30)};
+        Plate plate = new Plate(100);
+        for (Cat c : x) {
+            System.out.println(c);
+        }
+        System.out.println("\n" + plate);
+        System.out.println("***********************************************");
+        System.out.println("increase food by 100");
+        plate.increaseFood(50);
+        System.out.println(plate);
+        System.out.println("***********************************************");
+        System.out.println("Cats take food:");
+        for (Cat c : x) {
+            c.eat(plate);
+            System.out.println(c);
+            System.out.println(plate);
+        }
+    }
 }
+
+
